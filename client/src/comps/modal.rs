207667a04a27c97545e13a77a2afe53006c18bdd
Jesse Greenough <jesse.greenough84@gmail.com>
2024-10-17 22:06:38 -0700

    Update Modal component to accept reset callback for resetting inner children state on close.

--- client/src/comps/modal.rs
+++ client/src/comps/modal.rs
@@ -10,6 +10,8 @@ pub struct Props{
     pub children: Html,
     pub button_class: &'static str,
     pub button_icon: Html,
+    #[prop_or_default]
+    pub reset_state: Option<Callback<()>>,
 }
 
 pub fn toggle_modal(id: &str){
@@ -37,16 +39,23 @@ pub fn modal(Props {
      button_class,
      children,
      button_icon,
+     reset_state
  }: &Props) -> Html {
     let is_modal_open = use_state(|| false);
 
     let _toggle_modal = {
         let is_modal_open = is_modal_open.clone();
+        let reset_state = reset_state.clone();
         Callback::from(move |event: MouseEvent| {
             event.stop_propagation();
             event.prevent_default();
+
+            let reset_state = reset_state.clone();
             is_modal_open.set(!*is_modal_open);
             toggle_modal("modal_overlay");
+            if let Some(callback) = reset_state {
+                callback.emit(());
+            }
         })
     };
 
@@ -64,7 +73,7 @@ pub fn modal(Props {
                                     e.stop_propagation();
                                 })}
                             >
-                                <button class="return" onclick={_toggle_modal.clone()}>
+                                <button id="modal_return" class="return" onclick={_toggle_modal.clone()}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                                     </svg>
