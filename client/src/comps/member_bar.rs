71327083ce87e9c9a6a7e1cd848b81f05275373d
Jesse Greenough <jesse.greenough84@gmail.com>
2024-10-17 22:08:02 -0700

    Fix UI bug within Member Bar Add disabled styling for use on any unimplemented dynamic action buttons

--- client/src/comps/member_bar.rs
+++ client/src/comps/member_bar.rs
@@ -18,21 +18,21 @@ pub fn member_bar() -> Html {
         <>
             <div class={"member-bar"}>
                 <div onclick={onclick.clone()}>
-                    {get_random_svg(true, "member-icon", "55")}
+                    {get_random_svg(true, "member-icon", "40")}
                     <div>
                         <span>{&member_context.member.username}</span>
                         <span>{"Online"}</span>
                     </div>
                 </div>
                 // Microphone volume
-                <div class={"member-action-icon"}>
+                <div class={"member-action-icon disabled"}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-mic-mute-fill" viewBox="0 0 16 16">
                         <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4 4 0 0 0 12 8V7a.5.5 0 0 1 1 0zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a5 5 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4m3-9v4.879L5.158 2.037A3.001 3.001 0 0 1 11 3"/>
                         <path d="M9.486 10.607 5 6.12V8a3 3 0 0 0 4.486 2.607m-7.84-9.253 12 12 .708-.708-12-12z"/>
                     </svg>
                 </div>
                 // Headphone Volume
-                <div class={"member-action-icon"}>
+                <div class={"member-action-icon disabled"}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-headphones" viewBox="0 0 16 16">
                         <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5"/>
                     </svg>
