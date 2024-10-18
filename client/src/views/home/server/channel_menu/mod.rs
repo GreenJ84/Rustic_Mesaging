b2c32c1e5b3ee39e02692c9b94fad6c0f2d8c22f
Jesse Greenough <jesse.greenough84@gmail.com>
2024-10-18 00:37:20 -0700

    Implement ChannelForm on ChannelMenu for Channel Creation

--- client/src/views/home/server/channel_menu/mod.rs
+++ client/src/views/home/server/channel_menu/mod.rs
@@ -1,14 +1,16 @@
-
-use yew::{function_component, Html, html, use_context};
+use web_sys::MouseEvent;
+use yew::{Callback, function_component, Html, html, use_context};
 use crate::contexts::member_context::TMemberContext;
 use crate::models::channel::Channel;
 use crate::comps::member_bar::MemberBar;
 use crate::contexts::server_context::TServerContext;
+use crate::views::home::server::channel_menu::channel_form::ChannelForm;
 use crate::views::home::server::channel_menu::channel_menu_item::ChannelMenuItem;
 use crate::views::home::server::channel_menu::channel_menu_header::ChannelMenuHeader;
 
 pub(crate) mod channel_menu_header;
 pub(crate) mod channel_menu_item;
+pub(crate) mod channel_form;
 
 #[function_component(ChannelMenu)]
 pub fn channel_menu() -> Html {
@@ -28,13 +30,17 @@ pub fn channel_menu() -> Html {
                 {"Browse Channels"}
             </p>
             if is_owner {
-                 <p id={"add-channel"}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-patch-plus" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5"/>
-                        <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911z"/>
-                    </svg>
-                    {"Add new Channel"}
-                 </p>
+                 <ChannelForm
+                    entity={None}
+                    button_icon={html!(<>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-patch-plus" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5"/>
+                            <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911z"/>
+                        </svg>
+                        {"Add new Channel"}
+                    </>)}
+                    callback={Option::<Callback<MouseEvent>>::None}
+                 />
             }
             <hr/>
 
